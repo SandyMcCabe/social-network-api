1003310eea3b8cf8e58c436f8aba6a32e4513018
Sandy McCabe <sandytech13@gmail.com>
2022-01-09 13:52:27 -0600

    thought model complete

--- models/Thought.js
+++ models/Thought.js
@@ -1,41 +1,43 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+const reactSchema = require('./Reaction');
 
-var thoughtValidator = [
-    validate({
-      validator: 'isLength',
-      arguments: [1, 280],
-      message: 'Thought should be between 1 and 280 characters'
-    })
-];
-
-const thought = new Schema(
+const thoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
-      required: true,
-      trim: true,
-      validate: thoughtValidator
+      required: 'must leave a thought',
+      minlength: 1,
+      maxlength: 280,
     },
     createdAt: {
       type: Date,
       default: Date.now,
-//   ***********************uses a getter method to format the timestamp on query    
+      //   ***********************uses a getter method to format the timestamp on query    
+      get:timestamp => dateFormat(timestamp)
     
     },
     username: {
         type: String,
         required: true,
     },
-    reactions: {
     //  ***********************array of nexted documents created with the reaction schema
+    reactions: [
+        reactSchema
+    ],
+},
+{
+    toJSON: {
+        getters: true
     },
+    id: false
+});
     
 // retrieves the length of the thought's reactions array field on query
 UserSchema.virtual('reactionCount').get(function() {
-
+    return this.reactSchema.length;
 });
 
-const Thought = model('Thought', ThoughtSchema);
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
